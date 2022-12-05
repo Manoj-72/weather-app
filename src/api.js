a9af7711e9854374180ef1ab9c0f881247fc6d4c
Manoj-72 <99330600+Manoj-72@users.noreply.github.com>
2022-12-05 18:44:45 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '96101cdc28msh0e32d65b0cb149bp138f06jsnd31062a767ea',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+
+export const WEATHER_API_KEY = "5e85c0b006ce9e4ab8193f44cef9cf1a"
